02d24f4e0812439213e059d461adbdc83615e40e
Marian Narozhniak <narozhniakmarian@gmail.com>
2025-04-27 22:57:24 +0200

    head_mod_fix

--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -3,18 +3,22 @@
     openModalBtn: document.querySelector('[data-open-mobnav]'),
     closeModalBtn: document.querySelector('[data-clos-mod]'),
     modal: document.querySelector('[data-modal-mobnav]'),
+    menuLinks: document.querySelectorAll('[data-modal-mobnav] a'), // всі посилання в меню
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
   refs.modal.addEventListener('click', onBackdropClick);
 
+  refs.menuLinks.forEach(link => {
+    link.addEventListener('click', toggleModal);
+  });
+
   function toggleModal() {
     refs.modal.classList.toggle('isopen');
   }
 
   function onBackdropClick(event) {
-  
     if (event.target === refs.modal) {
       toggleModal();
     }
